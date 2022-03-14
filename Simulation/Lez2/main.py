12e0a7546e1d563d9141f97c3faa496a3b94596d
Alessio La Greca <alessio.lagreca13@gmail.com>
2022-03-14 13:53:59 +0100

    4° Lezione MAS

--- Simulation/Lez2/main.py
+++ Simulation/Lez2/main.py
@@ -1,5 +1,6 @@
 
 import random
+import numpy
 
 class Event():
     type = "a"
@@ -22,32 +23,78 @@ def pharmacy(sim_time, daily_working_time, exp_prescriptions_day, exp_prescr_tim
 
     events = []
 
-    current = Event()
-    current.type = "a"
-    current.time = get_next_delay(exp_prescriptions_day / daily_working_time)
+    e = Event()
+    e.type = "A"
+    e.time = get_next_delay(exp_prescriptions_day / daily_working_time)
     #if we expect 32 prescription daily, how many prescr can we expect for each minute?
     #We divide. Note that we decided to model time (daily_working_time) in minutes, but
     #it's just something we decided. We now just need to be coherent with our decision.
+    events.append(e)
 
-    while current.time < sim_time:
-        if current.type == "a":
 
+    while len(events) > 0:
+
+        # pick next event (that one of minimum time in events)
+        k = numpy.argmin([events[i].time for i in range(len(events))])
+        current = events[k]
+        events = events[:k] + events[k + 1:]
+
+        #print("Handling event at time ", current.time, " of type ", current.type)
+        #print("System status: pharmacist busy: ", busy, " queue: ", in_queue)
+
+        if current.type == "A":
             e = Event()
-            e.type = "a"
+            e.type = "A"
             e.time = current.time + get_next_delay(exp_prescriptions_day / daily_working_time)
-            events.append(e)
+
+            #if in_queue < 5
+            if e.time <= daily_working_time:    #if the shop is not closed...
+                events.append(e)
 
             if not busy:
+                #e = Event()
+                #e.type = "f"
+                #e.time = current.time + get_service_time(exp_prescr_time, stdev_prescr_time)
+                #events.append(e)
+
                 e = Event()
-                e.type = "f"
-                e.time = current.time + get_service_time(exp_prescr_time, stdev_prescr_time)
+                e.type = "S"
+                e.time = current.time
 
                 events.append(e)
 
             else:
                 in_queue = in_queue + 1
+            #else:
+                #lost_prescriptions += 1
+        elif current.type == "S":
 
+            busy = True
+
+            s_time = get_service_time(exp_prescr_time, stdev_prescr_time)
+
+            e = Event()
+            e.type = "F"
+            e.time = current.time + s_time
+
+            events.append(e)
+        elif current.type == "F":
+
+            busy = False
+            #now we can start working on a new prescription, if there is one
+            if in_queue > 0:
+                e = Event()
+                e.type = "S"
+                e.time = current.time
+
+                events.append(e)
+
+                in_queue = in_queue - 1
+    #return max(current.time, daily_working_time)
+    return current.time >= 510
 
 
 if __name__ == '__main__':
-    pass
+    t = [pharmacy(600, 480, 32, 10, 4) for i in range(1000)] #circa 490
+    media = numpy.mean(t)
+    print("Closing time: ", media)
