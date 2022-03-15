5a66fa98709cc2d4a0207e4b49a7d8914f86d5f0
Alessio La Greca <alessio.lagreca13@gmail.com>
2022-03-15 12:19:33 +0100

    4° lez Sim & GPU con Davide woof

---
+++ Simulation/ripassoProbabilita/main.py
@@ -0,0 +1,25 @@
+import itertools
+
+if __name__ == '__main__':
+    print("----------------")
+    S = list(itertools.permutations(range(5)))
+    [print(s) for s in S]
+    print("----------------")
+    A = list(filter(lambda L: L.index(0) == 0, S))   #horse 0 arrives first
+    [print(a) for a in A]
+    print("----------------")
+    B = list(filter(lambda L: L.index(1) < L.index(0), S)) #horse 1 arrives before horse 0
+    AorB = list(set.union(set(A), set(B)))                #union
+    [print(s) for s in AorB]
+    print("----------------")
+    AandB = list(set.intersection(set(A), set(B)))      #intersection
+    [print(s) for s in AandB]
+    print("----------------")
+    notA = list(set.difference(set(S), set(A)))
+    [print(s) for s in notA]
+    print("----------------")
+    print(len(A)/len(S))        #probability that an event will happen
+    print("----------------")
+
+
+
