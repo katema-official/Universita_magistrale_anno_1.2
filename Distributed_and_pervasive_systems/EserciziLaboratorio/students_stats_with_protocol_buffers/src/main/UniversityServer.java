063aaa3db736a20521714e3a996a59b279a31d05
Alessio La Greca <alessio.lagreca13@gmail.com>
2022-04-29 10:26:22 +0200

    questa lezione di laboratorio faceva schifo >:(

---
+++ Distributed_and_pervasive_systems/EserciziLaboratorio/students_stats_with_protocol_buffers/src/main/UniversityServer.java
@@ -0,0 +1,21 @@
+package main;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class UniversityServer {
+    public static void main(String argv[]) throws IOException {
+
+        //apro il server
+        ServerSocket serverSocket = new ServerSocket(9999);
+        Socket s = serverSocket.accept();
+
+        while(true) {
+
+            //Student student = Student.parseFrom(s.getInputStream());
+
+        }
+
+    }
+}
