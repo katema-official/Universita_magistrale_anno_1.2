f351937b80af9c646385a6be412a24b73123c335
Alessio La Greca <alessio.lagreca13@gmail.com>
2022-05-01 16:08:21 +0200

    prova3 funziona

---
+++ Distributed_and_pervasive_systems/EserciziLaboratorio/prova3/src/main/java/university/UniversityClient.java
@@ -0,0 +1,37 @@
+package university;
+
+import proto.classes.StudentOuterClass.Student;
+
+import java.io.IOException;
+import java.net.Socket;
+
+public class UniversityClient {
+    public static void main(String argv[]) throws IOException {
+        Socket clientSocket = new Socket("localhost", 9999);
+        Student s = Student.newBuilder()
+                .setName("Pippo")
+                .setSurname("Baudo")
+                .setYearOfBirth(1999)
+                .setLocation(Student.placeOfResidence.newBuilder()
+                        .setCountry("Italy")
+                        .setStreet("Via Dante Alighieri")
+                        .setNumber(34)
+                        .setCap(50405).build())
+                .addExams(Student.Exam.newBuilder()
+                        .setName("Svigruppo")
+                        .setMark(28)
+                        .setDateOfVerbalization("29/01/2022").build())
+                .addExams(Student.Exam.newBuilder()
+                        .setName("AIVG")
+                        .setMark(30)
+                        .setDateOfVerbalization("16/02/2022").build())
+                .addExams(Student.Exam.newBuilder()
+                        .setName("Progetto di Sistemi a Sensore")
+                        .setMark(32)
+                        .setDateOfVerbalization("23/01/2022").build()).build();
+
+        s.writeTo(clientSocket.getOutputStream());
+
+        clientSocket.close();
+    }
+}
--- Distributed_and_pervasive_systems/EserciziLaboratorio/prova3/src/main/java/university/UniversityServer.java
+++ Distributed_and_pervasive_systems/EserciziLaboratorio/prova3/src/main/java/university/UniversityServer.java
@@ -11,13 +11,30 @@ public class UniversityServer {
 
         //apro il server
         ServerSocket serverSocket = new ServerSocket(9999);
+        System.out.println("awaiting connenction...");
         Socket s = serverSocket.accept();
+        System.out.println("connection accepted");
 
-        while(true) {
 
-            Student student = Student.parseFrom(s.getInputStream());
+        Student student = Student.parseFrom(s.getInputStream());
 
+        System.out.println("Here are the stats of your student:\n" +
+                "Name: " + student.getName() + "\n" +
+                "Surname: " + student.getSurname() + "\n" +
+                "Year of Birth: " + student.getYearOfBirth() + "\n" +
+                "Place of residence: \n" +
+                "Country: "+ student.getLocation().getCountry() + "\n" +
+                "Street: "+ student.getLocation().getStreet() + "\n" +
+                "Number: "+ student.getLocation().getNumber() + "\n" +
+                "CAP: " + student.getLocation().getCap() + "\n");
+
+        for(int i = 0; i < student.getExamsCount(); i++){
+            System.out.println("Exam " + i + ":\n" +
+                    "Name: " + student.getExams(i).getName() + "\n" +
+                    "Mark: " + student.getExams(i).getMark() + "\n" +
+                    "Date of verbalization: " + student.getExams(i).getDateOfVerbalization() + "\n");
         }
 
+        s.close();
     }
 }
